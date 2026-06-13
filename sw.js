c58a1200f9c5bb06d534711baa81316f92763afc
alireza <alirezasoltani522>
2026-06-13 08:29:11 +0330

    .

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "net-check-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/offline.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match("/offline.html"))
+  );
+});
